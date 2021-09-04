23689c42d3a84ee8c5f42c567494945d80ae13ce
rohitinsuide <rohit.n@insuide.com>
2021-09-05 01:20:16 +0530

    data structure match

--- index.js
+++ index.js
@@ -23,29 +23,53 @@ const client = new MongoClient('mongodb://localhost:27017');
 async function main() {
     await client.connect();
     console.log('Connected successfully to mongo');
-    const collection = client.db('insuide').collection('wayedges');
+    const database = client.db('insuide');
     
-    const wayedges = await collection.find({appname:argv.appName}).toArray();
+    const edge_collection = database.collection('wayedges'),
+          point_collection = database.collection('waypoints');
+
+    const result = {
+        "success": true,
+        "edges": [],
+        "markers": []
+    };
+
+    const appname = argv.appName;
+    
+    const wayedges = await edge_collection.find({appname}).toArray();
     
     console.log(`Going to process ${wayedges.length} line-data`);
 
 
     for(let i = 0; i < wayedges.length; i++){
 
-        const { from_point, to_point } = wayedges[i];
+        const { _id, from_point, to_point } = wayedges[i];
         
         const isLineBi = wayedges.find( elem => elem.from_point === to_point && elem.to_point === from_point );
 
-        wayedges[i].biDirectional = isLineBi ? true : false;
+        const body = { _id, from_point, to_point, biDirectional: isLineBi ? true : false };
+
+        result.edges.push(body);
+
+    }
+
+    const waypoints =  await point_collection.find({appname}).toArray();
+
+    for(let i = 0; i < waypoints.length; i++){
+
+        const {_id, lat, level, lng, terminal} = waypoints[i];
+
+        result.markers.push({_id, lat, level, lng, terminal});
 
     }
 
 
-    writeFileSync('wayedges_export.json', JSON.stringify(wayedges));
+
+    writeFileSync('result.json', JSON.stringify(result));
 
 
 
-    return "\n File exported to 'wayedges_export.json' \n";
+    return "\n File exported to 'result.json' \n";
   }
 
 

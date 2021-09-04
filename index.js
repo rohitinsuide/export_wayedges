c4fc81bc2f5b2d28ec077ef1d623b208fe25c72f
rohitinsuide <rohit.n@insuide.com>
2021-09-04 18:58:43 +0530

    test stage

--- index.js
+++ index.js
@@ -1,25 +1,49 @@
 "use strict";
 
-const wayedges = require('./wayedges_test.json');
 
-const { writeFileSync } = require('fs');
-
-console.log(`Going to process ${wayedges.length} line-data`);
+const { MongoClient } = require('mongodb');
 
 
-for(let i = 0; i < wayedges.length; i++){
+const client = new MongoClient('mongodb://localhost:27017');
 
-    const { from_point, to_point } = wayedges[i];
+
+async function main() {
+    await client.connect();
+    console.log('Connected successfully to server');
+    const collection = client.db('insuide').collection('wayedges');
+    
+    const wayedges = await collection.find({appname:'bud'}).toArray();
+    
+    console.log(wayedges);
+
+    return 'done.';
+  }
+
+
+  main()
+  .then(console.log)
+  .catch(console.error)
+  .finally(() => client.close());
+
+
+// const { writeFileSync } = require('fs');
+
+// console.log(`Going to process ${wayedges.length} line-data`);
+
+
+// for(let i = 0; i < wayedges.length; i++){
+
+//     const { from_point, to_point } = wayedges[i];
 
     
-    const isLineBi = wayedges.find( elem => elem.from_point === to_point && elem.to_point === from_point );
+//     const isLineBi = wayedges.find( elem => elem.from_point === to_point && elem.to_point === from_point );
 
 
-    wayedges[i].biDirectional = isLineBi ? true : false;
+//     wayedges[i].biDirectional = isLineBi ? true : false;
 
 
-}
+// }
 
 
-writeFileSync('wayedges_export.json', JSON.stringify(wayedges));
-console.log("\n File exported to 'wayedges_export.json' \n");
+// writeFileSync('wayedges_export.json', JSON.stringify(wayedges));
+// console.log("\n File exported to 'wayedges_export.json' \n");
